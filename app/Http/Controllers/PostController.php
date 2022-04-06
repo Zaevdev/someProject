3e7244a07f558495177666530259adfa31a12218
Zaevdev <zaevdev@gmail.com>
2022-04-06 20:45:02 +0300

    add model and method add in db

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,14 +2,52 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
 
     public function index()
     {
-        return 'This is page of PostController';
+        $post = Post::where('is_published', 1)->get();
+        foreach ($post as $posts) {
+            echo $posts->title . '</br>';
+        }
+
     }
+
+    public function create()
+    {
+        $postsArr = [
+            [
+                'title' => 'title of post from phpstorm',
+                'content' => 'some interesting content',
+                'image' => 'image1.jpg',
+                'likes' => 0,
+                'is_published' => 1,
+            ],
+            [
+                'title' => 'new second title',
+                'content' => 'some interesting content, version 2.0',
+                'image' => 'image2.jpg',
+                'likes' => 0,
+                'is_published' => 1,
+            ],
+            [
+                'title' => 'some title',
+                'content' => 'so-so content',
+                'image' => 'image3.jpg',
+                'likes' => 0,
+                'is_published' => 1,
+            ],
+        ];
+
+        foreach ($postsArr as $item) {
+            Post::create($item);
+        }
+
+        dd('created');
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/index', [PostController::class, 'index']);
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/create', [PostController::class, 'create']);
