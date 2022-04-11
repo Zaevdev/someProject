aba4c0a99161775fc033e121bfe960da92c1486c
Zaevdev <zaevdev@gmail.com>
2022-04-11 19:05:15 +0300

    converted to single-method controllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\Post\CreateController;
+use App\Http\Controllers\Post\DeleteController;
+use App\Http\Controllers\Post\EditController;
+use App\Http\Controllers\Post\IndexController;
+use App\Http\Controllers\Post\ShowController;
+use App\Http\Controllers\Post\StoreController;
+use App\Http\Controllers\Post\UpdateController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\MainController;
 
@@ -20,17 +26,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/posts', [PostController::class, 'index'])->name('post.index');
-Route::get('/posts/create', [PostController::class, 'create'])->name('post.create');
-Route::post('/posts', [PostController::class, 'store'])->name('post.store');
-Route::get('/posts/{post}', [PostController::class, 'show'])->name('post.show');
-Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('post.edit');
-Route::patch('/posts/{post}', [PostController::class, 'update'])->name('post.update');
-Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('post.destroy');
+Route::get('/posts', [IndexController::class, '__invoke'])->name('post.index');
+Route::get('/posts/create', [CreateController::class, '__invoke'])->name('post.create');
+Route::post('/posts', [StoreController::class, '__invoke'])->name('post.store');
+Route::get('/posts/{post}', [ShowController::class, '__invoke'])->name('post.show');
+Route::get('/posts/{post}/edit', [EditController::class, '__invoke'])->name('post.edit');
+Route::patch('/posts/{post}', [UpdateController::class, '__invoke'])->name('post.update');
+Route::delete('/posts/{post}', [DeleteController::class, '__invoke'])->name('post.destroy');
 
-Route::get('/posts/update', [PostController::class, 'update']);
-Route::get('/posts/delete', [PostController::class, 'delete']);
-Route::get('/posts/first_or_create', [PostController::class, 'firstOrCreate']);
 
 Route::get('/about', [AboutController::class, 'index'])->name('about.index');
 Route::get('/main', [MainController::class, 'index'])->name('main.index');
